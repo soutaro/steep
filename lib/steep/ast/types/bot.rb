a7aa169756eedd3648db9c38cfaacd340edf95d7
Soutaro Matsumoto <matsumoto@soutaro.com>
2018-04-05 01:46:47 +0900

    Add top and bot types
    # Conflicts:
    #	lib/steep.rb

---
+++ lib/steep/ast/types/bot.rb
@@ -0,0 +1,37 @@
+module Steep
+  module AST
+    module Types
+      class Bot
+        attr_reader :location
+
+        def initialize(location: nil)
+          @location = location
+        end
+
+        def ==(other, ignore_location: false)
+          other.is_a?(Bot) && (ignore_location || !other.location || !location || other.location == location)
+        end
+
+        def hash
+          self.class.hash
+        end
+
+        def eql?(other)
+          __send__(:==, other, ignore_location: true)
+        end
+
+        def subst(s)
+          self
+        end
+
+        def to_s
+          "⟘"
+        end
+
+        def free_variables
+          Set.new
+        end
+      end
+    end
+  end
+end
