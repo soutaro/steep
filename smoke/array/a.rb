ea6e654f62649653779238ba00b17a3a34f1e47c
Soutaro Matsumoto <matsumoto@soutaro.com>
2018-05-20 23:05:59 +0900

    Update builtin

--- smoke/array/a.rb
+++ smoke/array/a.rb
@@ -7,16 +7,17 @@ a[1] = 3
 a[2] = "foo"
 
 # @type var i: Integer
-# @type var s: String
-
+# !expects IncompatibleAssignment: lhs_type=::Integer, rhs_type=(::Integer | ::NilClass)
 i = a[0]
 
-# !expects IncompatibleAssignment: lhs_type=::String, rhs_type=::Integer
+# @type var s: String
+# !expects IncompatibleAssignment: lhs_type=::String, rhs_type=(::Integer | ::NilClass)
 s = a[1]
 
 
 b = ["a", "b", "c"]
 
+# !expects IncompatibleAssignment: lhs_type=::String, rhs_type=(::NilClass | ::String)
 s = b[0]
-# !expects IncompatibleAssignment: lhs_type=::Integer, rhs_type=::String
+# !expects IncompatibleAssignment: lhs_type=::Integer, rhs_type=(::NilClass | ::String)
 i = b[1]
