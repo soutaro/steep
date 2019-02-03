cb4759801d5e0e8edc7240575aadfac12a3df1a9
Soutaro Matsumoto <matsumoto@soutaro.com>
2019-02-04 02:08:43 +0900

    Use hint for `or` node typing

--- test/type_construction_test.rb
+++ test/type_construction_test.rb
@@ -6010,4 +6010,38 @@ end
       error.is_a?(Steep::Errors::ArgumentTypeMismatch)
     end
   end
+
+  def test_or_nil_unwrap
+    checker = new_subtyping_checker()
+
+    source = parse_ruby(<<-EOF)
+# @type var x: Array<Integer>?
+x = nil
+y = x || []
+    EOF
+
+    typing = Typing.new
+    annotations = source.annotations(block: source.node, builder: checker.builder, current_module: Namespace.root)
+    const_env = ConstantEnv.new(builder: checker.builder, context: nil)
+    type_env = TypeEnv.build(annotations: annotations,
+                             subtyping: checker,
+                             const_env: const_env,
+                             signatures: checker.builder.signatures)
+
+    construction = TypeConstruction.new(checker: checker,
+                                        source: source,
+                                        annotations: annotations,
+                                        type_env: type_env,
+                                        block_context: nil,
+                                        self_type: Types::Name.new_instance(name: "::Object"),
+                                        method_context: nil,
+                                        typing: typing,
+                                        module_context: nil,
+                                        break_context: nil)
+
+    construction.synthesize(source.node)
+
+    assert_empty typing.errors
+    assert_equal parse_type("::Array<::Integer>"), type_env.lvar_types[:y]
+  end
 end
