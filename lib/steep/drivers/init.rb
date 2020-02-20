0dbc2a01ea0791829faa7a9ff56c87756067c6c0
Masataka Pocke Kuwabara <kuwabara@pocke.me>
2020-02-20 23:17:28 +0900

    Remove trailing spaces from Steepfile that generated by `steep init`

--- lib/steep/drivers/init.rb
+++ lib/steep/drivers/init.rb
@@ -10,21 +10,21 @@ module Steep
       TEMPLATE = <<~EOF
       # target :lib do
       #   signature "sig"
-      # 
+      #
       #   check "lib"                       # Directory name
       #   check "Gemfile"                   # File name
       #   check "app/models/**/*.rb"        # Glob
-      #   # ignore "lib/templates/*.rb"        
-      #   
+      #   # ignore "lib/templates/*.rb"
+      #
       #   # library "pathname", "set"       # Standard libraries
       #   # library "strong_json"           # Gems
       # end
 
       # target :spec do
       #   signature "sig", "sig-private"
-      # 
+      #
       #   check "spec"
-      # 
+      #
       #   # library "pathname", "set"       # Standard libraries
       #   # library "rspec"
       # end
