9a3ae85216e2ef486431ecbf89e8e35e1bf09860
Soutaro Matsumoto <matsumoto@soutaro.com>
2020-03-15 00:23:15 +0900

    Add LocalVariableTypeEnv

--- lib/steep.rb
+++ lib/steep.rb
@@ -65,6 +65,7 @@ require "steep/type_inference/send_args"
 require "steep/type_inference/block_params"
 require "steep/type_inference/constant_env"
 require "steep/type_inference/type_env"
+require "steep/type_inference/local_variable_type_env"
 require "steep/ast/types"
 
 require "steep/project"
