8b34b2ca391bcf1e8a61a29cfc7b69dee2e63a3f
Soutaro Matsumoto <matsumoto@soutaro.com>
2020-12-30 23:44:43 +0900

    Fix smoke/lambda

--- smoke/lambda/a.rb
+++ smoke/lambda/a.rb
@@ -1,6 +1,5 @@
 # @type var a: Integer
 
-# !expects IncompatibleAssignment: lhs_type=::Integer, rhs_type=::String
 a = -> (x, y) do
   # @type var x: String
   # @type var y: String
@@ -8,10 +7,8 @@ a = -> (x, y) do
 end["foo", "bar"]
 
 # @type var b: ^(Integer) -> Integer
-# !expects IncompatibleAssignment: lhs_type=^(::Integer) -> ::Integer, rhs_type=::Proc
 b = lambda do |x|
-  # !expects NoMethodError: type=nil, method=+
-  x + 1 
+  x + 1
 end
 
 # @type var c: ^(Integer) -> Integer
