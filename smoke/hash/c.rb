59cec16467364a72975a23233c086b508bc93e4f
Soutaro Matsumoto <matsumoto@soutaro.com>
2020-12-30 23:54:00 +0900

    Fix smoke/hash

--- smoke/hash/c.rb
+++ smoke/hash/c.rb
@@ -2,14 +2,11 @@
 params = _ = nil
 
 id = params[:id]
-# !expects NoMethodError: type=::Integer, method=abcdefg
 id.abcdefg
 
 name = params[:name]
-# !expects NoMethodError: type=::String, method=abcdefg
 name.abcdefg
 
-# !expects NoMethodError: type=(::Integer | ::String), method=abcdefg
 params[(_=nil) ? :id : :name].abcdefg
 
 # @type var controller: Controller
--- smoke/hash/d.rb
+++ smoke/hash/d.rb
@@ -2,5 +2,4 @@
 
 params = { id: 30, name: "Matz" }
 
-# !expects IncompatibleAssignment: lhs_type={ :id => ::Integer, :name => ::String }, rhs_type={ :email => ::String, :id => ::String, :name => ::String }
 params = { id: "30", name: "foo", email: "matsumoto@soutaro.com" }
