462603850096d9dca2882175edc9742119e34eb8
Soutaro Matsumoto <matsumoto@soutaro.com>
2020-12-30 23:26:02 +0900

    Fix smoke/skip

--- smoke/skip/skip.rb
+++ smoke/skip/skip.rb
@@ -5,11 +5,9 @@ end
 # @type var foo: String
 
 foo = _ = begin
-  # !expects NoMethodError: type=::Object, method=no_such_method
   self.no_such_method
 end
 
 foo = __any__ = begin
-  # !expects NoMethodError: type=::Object, method=no_such_method
   self.no_such_method
 end
