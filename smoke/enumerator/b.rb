004496edae9a20f333161bddae8fdbdcc66f309b
Soutaro Matsumoto <matsumoto@soutaro.com>
2020-12-30 23:57:26 +0900

    Fix smoke/enumerator

--- smoke/enumerator/b.rb
+++ smoke/enumerator/b.rb
@@ -3,20 +3,17 @@
 
 a = [1]
 
-# !expects* UnresolvedOverloading:
 b = a.each.with_object([]) do |i, xs|
   # @type var xs: String
   xs << i.to_s
 end
 
-# !expects IncompatibleAssignment: lhs_type=::Array[::Integer], rhs_type=::Array[::String]
 c = a.each.with_object([]) do |i, xs|
   # @type var xs: Array[String]
   xs << i.to_s
 end
 
 # @type var d: String
-# !expects IncompatibleAssignment: lhs_type=::String, rhs_type=::Array[untyped]
 d = a.each.with_object([]) do |i, xs|
   xs << i.to_s
 end
