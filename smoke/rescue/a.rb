edbd4eba97ec28787eadeab7c69702c74b42cf02
Soutaro Matsumoto <matsumoto@soutaro.com>
2020-12-30 23:28:21 +0900

    Fix smoke/rescue

--- smoke/rescue/a.rb
+++ smoke/rescue/a.rb
@@ -1,6 +1,5 @@
 # @type var a: Integer
 
-# !expects IncompatibleAssignment: lhs_type=::Integer, rhs_type=(::String | ::Integer)
 a = begin
       'foo'
     rescue
@@ -9,12 +8,10 @@ a = begin
 
 # @type var b: Integer
 
-# !expects IncompatibleAssignment: lhs_type=::Integer, rhs_type=(::String | ::Integer)
 b = 'foo' rescue 1
 
 # @type var c: Integer
 
-# !expects IncompatibleAssignment: lhs_type=::Integer, rhs_type=(::String | ::Symbol | ::Integer)
 c = begin
       'foo'
     rescue RuntimeError
@@ -25,7 +22,6 @@ c = begin
 
 # @type var e: Integer
 
-# !expects IncompatibleAssignment: lhs_type=::Integer, rhs_type=(::Array[::Integer] | ::Symbol | ::Integer)
 e = begin
       'foo'
     rescue RuntimeError
@@ -38,7 +34,6 @@ e = begin
 
 # @type method foo: (String) -> String
 
-# !expects MethodBodyTypeMismatch: method=foo, expected=::String, actual=(::Integer | ::String)
 def foo(a)
   10
 rescue
