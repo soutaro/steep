b72ea1e11c61cb4434a2e42626633197f3e5f331
Soutaro Matsumoto <matsumoto@soutaro.com>
2020-12-30 23:48:16 +0900

    Fix smoke/integer

--- smoke/integer/a.rb
+++ smoke/integer/a.rb
@@ -1,33 +1,26 @@
 # @type var x: Numeric
 x = _ = 1
 integer_1 = Integer(x)
-# !expects NoMethodError: type=::Integer, method=foo
 integer_1.foo
 
 integer_2 = Integer("1")
-# !expects NoMethodError: type=::Integer, method=foo
 integer_2.foo
 
 class WithToInt
   def to_int; 1; end
 end
 integer_3 = Integer(WithToInt.new)
-# !expects NoMethodError: type=::Integer, method=foo
 integer_3.foo
 
 class WithToI
   def to_i; 1; end
 end
 integer_4 = Integer(WithToI.new)
-# !expects NoMethodError: type=::Integer, method=foo
 integer_4.foo
 
 integer_5 = Integer("10", 2)
-# !expects NoMethodError: type=::Integer, method=foo
 integer_5.foo
 
-# !expects* UnresolvedOverloading: receiver=::Object, method_name=Integer,
 Integer(Object.new)
 
-# !expects* UnresolvedOverloading: receiver=::Object, method_name=Integer,
 Integer(nil)
