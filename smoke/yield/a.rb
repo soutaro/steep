86b7e377e101f077772c9bf35b52897fc5c82b50
Soutaro Matsumoto <matsumoto@soutaro.com>
2020-12-30 23:15:29 +0900

    Fix smoke/yield

--- smoke/yield/a.rb
+++ smoke/yield/a.rb
@@ -3,16 +3,13 @@ class A
   def foo()
     # @type var x: String
 
-    # !expects IncompatibleAssignment: lhs_type=::String, rhs_type=::Integer
     x = yield(3)
 
-    # !expects IncompatibleAssignment: lhs_type=::Integer, rhs_type=::String
     yield(x)
   end
 
   # @type method bar: () -> untyped
   def bar()
-    # !expects UnexpectedYield
     yield 4
   end
 end
