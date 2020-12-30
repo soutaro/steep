fd13954cbbae3d7d9cdb21bf086d5d28d0f96f00
Soutaro Matsumoto <matsumoto@soutaro.com>
2020-12-31 00:06:48 +0900

    Fix smoke/array

--- smoke/array/a.rb
+++ smoke/array/a.rb
@@ -3,19 +3,16 @@
 a = []
 a[1] = 3
 
-# !expects* UnresolvedOverloading: receiver=::Array[::Integer], method_name=[]=,
 a[2] = "foo"
 
 # @type var i: Integer
 i = a[0]
 
 # @type var s: String
-# !expects IncompatibleAssignment: lhs_type=::String, rhs_type=::Integer
 s = a[1]
 
 
 b = ["a", "b", "c"]
 
 s = b[0]
-# !expects IncompatibleAssignment: lhs_type=::Integer, rhs_type=::String
 i = b[1]
--- smoke/array/c.rb
+++ smoke/array/c.rb
@@ -3,5 +3,4 @@
 x = ["foo"]
 x = [nil]
 
-# !expects IncompatibleAssignment: lhs_type=::Array[(::String | nil)], rhs_type=::Array[::Integer]
 x = [1]
