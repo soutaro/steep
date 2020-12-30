ee4ef9235843805c6381ee562359fe90854eb34c
Soutaro Matsumoto <matsumoto@soutaro.com>
2020-12-31 00:10:06 +0900

    Fix smoke/and

--- smoke/and/a.rb
+++ smoke/and/a.rb
@@ -3,9 +3,6 @@
 
 a = "foo"
 
-# !expects IncompatibleAssignment: lhs_type=::String, rhs_type=(::String | nil)
 b = a && a.to_str
 
-# !expects IncompatibleAssignment: lhs_type=::Integer, rhs_type=(::String | nil)
 c = a && a.to_str
-
