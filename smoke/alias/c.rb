5c8b0beee704a3d81287d5fb53dd9ea15dec0cd5
Soutaro Matsumoto <matsumoto@soutaro.com>
2020-12-31 00:11:49 +0900

    Fix smoke/alias

--- smoke/alias/c.rb
+++ smoke/alias/c.rb
@@ -1,10 +1,8 @@
 # @type var x: String
 
-# !expects IncompatibleAssignment: lhs_type=::String, rhs_type=::Integer
 x = AliasMethodArg.new.foo(:foo)
 
 # @type var name: name
 name = :bar
 
-# !expects IncompatibleAssignment: lhs_type=::String, rhs_type=::Integer
 x = AliasMethodArg.new.foo(name)
