3f483e60343268f4adf41560f287c529f14e59ed
Soutaro Matsumoto <matsumoto@soutaro.com>
2021-01-28 18:48:00 +0900

    Print RBS validation results through LSPFormatter

--- lib/steep/range_extension.rb
+++ lib/steep/range_extension.rb
@@ -1,3 +1,18 @@
+class RBS::Location
+  def as_lsp_range
+    {
+      start: {
+          line: start_line - 1,
+          character: start_column
+      },
+      end: {
+        line: end_line - 1,
+        character: end_column
+      }
+    }
+  end
+end
+
 class Parser::Source::Range
   def as_lsp_range
     {
