60698156e2f4acf0a636ba676cda21ba429c9041
Soutaro Matsumoto <matsumoto@soutaro.com>
2021-02-23 00:30:44 +0900

    Use SignatureController in Drivers::Validate

--- lib/steep/drivers/validate.rb
+++ lib/steep/drivers/validate.rb
@@ -17,29 +17,44 @@ module Steep
         loader = Project::FileLoader.new(project: project)
         loader.load_signatures()
 
-        type_check(project)
+        any_error = false
 
         project.targets.each do |target|
-          Steep.logger.tagged "target=#{target.name}" do
-            case (status = target.status)
-            when Project::Target::SignatureErrorStatus
-              formatter = Diagnostic::LSPFormatter.new
-              diagnostics = status.errors.group_by {|e| e.location.buffer }.transform_values do |errors|
-                errors.map {|error| formatter.format(error) }
-              end
+          loader = Project::Target.construct_env_loader(options: target.options)
+          controller = SignatureController.load_from(loader)
 
-              diagnostics.each do |buffer, ds|
-                printer = DiagnosticPrinter.new(stdout: stdout, buffer: buffer)
-                ds.each do |d|
-                  printer.print(d)
-                  stdout.puts
-                end
-              end
+          changes = target.signature_files.each.with_object({}) do |(path, file), changes|
+            changes[path] = [
+              SignatureController::ContentChange.new(range: nil, text: file.content)
+            ]
+          end
+          controller.update(changes)
+
+          errors = case controller.status
+                   when SignatureController::ErrorStatus
+                     controller.status.diagnostics
+                   when SignatureController::LoadedStatus
+                     check = Subtyping::Check.new(factory: AST::Types::Factory.new(builder: controller.current_builder))
+                     Signature::Validator.new(checker: check).tap {|v| v.validate() }.each_error.to_a
+                   end
+
+          any_error ||= !errors.empty?
+
+          formatter = Diagnostic::LSPFormatter.new
+          diagnostics = errors.group_by {|e| e.location.buffer }.transform_values do |errors|
+            errors.map {|error| formatter.format(error) }
+          end
+
+          diagnostics.each do |buffer, ds|
+            printer = DiagnosticPrinter.new(stdout: stdout, buffer: buffer)
+            ds.each do |d|
+              printer.print(d)
+              stdout.puts
             end
           end
         end
 
-        project.targets.all? {|target| target.status.is_a?(Project::Target::TypeCheckStatus) } ? 0 : 1
+        any_error ? 1 : 0
       end
     end
   end
