1228da9317f2565b857029eb8f3fc056d2314215
Soutaro Matsumoto <matsumoto@soutaro.com>
2021-02-24 00:20:26 +0900

    Update SignatureWorker

--- lib/steep/server/signature_worker.rb
+++ lib/steep/server/signature_worker.rb
@@ -2,94 +2,131 @@ module Steep
   module Server
     class SignatureWorker < BaseWorker
       attr_reader :queue
-      attr_reader :last_target_validated_at
+      attr_reader :controllers
 
       def initialize(project:, reader:, writer:, queue: Queue.new)
         super(project: project, reader: reader, writer: writer)
 
         @queue = queue
-        @last_target_validated_at = {}
-      end
-
-      def validate_signature_if_required(request)
-        path = source_path(URI.parse(request[:params][:textDocument][:uri]))
-
-        project.targets.each do |target|
-          if target.signature_file?(path)
-            enqueue_target target: target, timestamp: Time.now
-          end
+        @changes = {}
+        @mutex = Mutex.new
+        @controllers = project.targets.each.with_object({}) do |target, hash|
+          loader = Project::Target.construct_env_loader(options: target.options)
+          hash[target.name] = SignatureController.load_from(loader)
         end
       end
 
-      def enqueue_target(target:, timestamp:)
-        Steep.logger.debug "queueing target #{target.name}@#{timestamp}"
-        last_target_validated_at[target] = timestamp
-        queue << [:validate, [target, timestamp]]
+      def push_change
+        @mutex.synchronize do
+          yield @changes
+        end
+      end
+
+      def pop_change
+        changes = {}
+        @mutex.synchronize do
+          changes.merge!(@changes)
+          @changes.clear
+        end
+        if block_given?
+          yield changes
+        else
+          changes
+        end
+      end
+
+      def enqueue_validation()
+        queue << [:validate, []]
       end
 
       def enqueue_symbol(id:, query:)
-        Steep.logger.debug "queueing symbol #{query} (#{id})"
+        Steep.logger.info "Queueing symbol #{query} (#{id})"
         queue << [:symbol, [id, query]]
+      end
+
+      def load_project_files()
+        push_change do |changes|
+          project.targets.each do |target|
+            target_changes = target.signature_files.each.with_object({}) do |(path, file), hash|
+              changes[path] = [SignatureController::ContentChange.new(text: file.content)]
+            end
+          end
+        end
       end
 
       def handle_request(request)
         case request[:method]
         when "initialize"
-          # Start type checking.
-          project.targets.each do |target|
-            enqueue_target(target: target, timestamp: Time.now)
-          end
-
+          load_project_files()
+          enqueue_validation()
           writer.write({ id: request[:id], result: nil})
 
         when "textDocument/didChange"
-          update_source(request)
-          validate_signature_if_required(request)
+          collect_changes(request)
+          enqueue_validation()
+
         when "workspace/symbol"
           enqueue_symbol(query: request[:params][:query], id: request[:id])
         end
       end
 
-      def validate_signature(target, timestamp:)
-        Steep.logger.info "Starting signature validation: #{target.name} (#{timestamp})..."
+      def collect_changes(request)
+        push_change do |changes|
+          path = project.relative_path(Pathname(URI.parse(request[:params][:textDocument][:uri]).path))
+          version = request[:params][:textDocument][:version]
+          Steep.logger.info { "Updating source: path=#{path}, version=#{version}..." }
 
-        target.type_check(target_sources: [], validate_signatures: true)
+          changes[path] ||= []
+          request[:params][:contentChanges].each do |change|
+            changes[path] << SignatureController::ContentChange.new(
+              range: change[:range]&.yield_self {|range|
+                [
+                  range[:start].yield_self {|pos| SignatureController::Position.new(line: pos[:line] + 1, column: pos[:character]) },
+                  range[:end].yield_self {|pos| SignatureController::Position.new(line: pos[:line] + 1, column: pos[:character]) }
+                ]
+              },
+              text: change[:text]
+            )
+          end
+        end
+      end
 
-        Steep.logger.info "Finished signature validation: #{target.name} (#{timestamp})"
+      def validate_signature(changes:)
+        Steep.logger.info { "#validate_signature: changes=#{changes.keys.join(", ")}"}
+        all_diagnostics = {}
 
-        diagnostics = case status = target.status
-                      when Project::Target::SignatureErrorStatus
-                        error_hash = status.errors.group_by {|error| error.location.buffer.name }
-                        formatter = Diagnostic::LSPFormatter.new
+        project.targets.each do |target|
+          controller = controllers[target.name]
+          target_changes = changes.filter {|path, _| target.possible_signature_file?(path) }
 
-                        target.signature_files.each_key.with_object({}) do |path, hash|
-                          errors = error_hash[path] || []
-                          hash[path] = errors.map {|error| formatter.format(error) }
-                        end
-                      when Project::Target::TypeCheckStatus
-                        target.signature_files.each_key.with_object({}) do |path, hash|
-                          hash[path] = []
-                        end
-                      else
-                        Steep.logger.info "Unexpected target status: #{status.class}"
-                        {}
-                      end
+          if !target_changes.empty?
+            controller.update(target_changes) unless target_changes.empty?
 
-        diagnostics.each do |path, diags|
+            diagnostics = case controller.status
+                          when SignatureController::ErrorStatus
+                            controller.status.diagnostics
+                          when SignatureController::LoadedStatus
+                            check = Subtyping::Check.new(factory: AST::Types::Factory.new(builder: controller.current_builder))
+                            Signature::Validator.new(checker: check).tap {|v| v.validate() }.each_error.to_a
+                          end.group_by {|error| error.location.buffer.name }
+
+            formatter = Diagnostic::LSPFormatter.new
+            controller.current_files.each_key do |path|
+              all_diagnostics[path] ||= []
+              all_diagnostics[path].push(*(diagnostics[path] || []).map {|error| formatter.format(error) })
+            end
+          end
+        end
+
+        all_diagnostics.each do |path, diagnostics|
+          Steep.logger.info { "Reporting #{diagnostics.size} diagnostics for #{path}" }
           writer.write(
             method: :"textDocument/publishDiagnostics",
             params: LSP::Interface::PublishDiagnosticsParams.new(
               uri: URI.parse(project.absolute_path(path).to_s).tap {|uri| uri.scheme = "file"},
-              diagnostics: diags
+              diagnostics: diagnostics.uniq
             )
           )
-        end
-      end
-
-      def active_job?(target, timestamp)
-        if last_target_validated_at[target] == timestamp
-          sleep 0.1
-          last_target_validated_at[target] == timestamp
         end
       end
 
@@ -97,15 +134,14 @@ module Steep
         provider = Index::SignatureSymbolProvider.new()
 
         project.targets.each do |target|
-          case target.status
-          when Project::Target::TypeCheckStatus
-            index = Index::RBSIndex.new()
+          controller = controllers[target.name]
 
-            builder = Index::RBSIndex::Builder.new(index: index)
-            builder.env(target.status.environment)
+          index = Index::RBSIndex.new()
 
-            provider.indexes << index
-          end
+          builder = Index::RBSIndex::Builder.new(index: index)
+          builder.env(controller.current_env)
+
+          provider.indexes << index
         end
 
         symbols = provider.query_symbol(query)
@@ -136,20 +172,21 @@ module Steep
       end
 
       def handle_job(job)
+        Steep.logger.info { "#handle_job: #{job.inspect}"}
         action, data = job
 
         case action
         when :validate
-          target, timestamp = data
-
-          if active_job?(target, timestamp)
-            validate_signature(target, timestamp: timestamp)
-          else
-            Steep.logger.info "Skipping signature validation: #{target.name}, queued timestamp=#{timestamp}, latest timestamp=#{last_target_validated_at[target]}"
+          changes = pop_change()
+          Steep.measure "Signature validation", level: :info do
+            validate_signature(changes: changes)
           end
+
         when :symbol
           id, query = data
-          handle_workspace_symbol(query: query, id: id)
+          Steep.measure "Symbol list generation", level: :info do
+            handle_workspace_symbol(query: query, id: id)
+          end
         end
       end
     end
