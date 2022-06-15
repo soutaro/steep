ca2851dc9df930eae69fc4ab34330000e7d7a8a6
Soutaro Matsumoto <matsumoto@soutaro.com>
2022-06-15 10:07:25 +0900

    Fix PathHelper
    * Add test
    * Use `URI::File.build`

--- lib/steep/path_helper.rb
+++ lib/steep/path_helper.rb
@@ -2,18 +2,21 @@ module Steep
   module PathHelper
     module_function
 
-    def to_pathname(uri)
-      path = URI.parse(uri).path
-      path.sub!(%r{^/([a-zA-Z])(:|%3A)//?}i, '\1:/') if Gem.win_platform?
-      Pathname(path)
+    def to_pathname(uri, dosish: Gem.win_platform?)
+      uri = URI.parse(uri)
+      if uri.scheme == "file"
+        path = uri.path
+        path.sub!(%r{^/([a-zA-Z])(:|%3A)//?}i, '\1:/') if dosish
+        Pathname(path)
+      end
     end
 
-    def to_uri(path)
+    def to_uri(path, dosish: Gem.win_platform?)
       str_path = path.to_s
-      if Gem.win_platform?
+      if dosish
         str_path.insert(0, "/") if str_path[0] != "/"
       end
-      URI.parse(str_path).tap {|uri| uri.scheme = "file"}
+      URI::File.build(path: str_path)
     end
   end
 end
---
+++ test/path_helper_test.rb
@@ -0,0 +1,25 @@
+require_relative "test_helper"
+
+class PathHelperTest < Minitest::Test
+  include Steep
+  include TestHelper
+  include PathHelper
+
+  def test_to_pathname
+    # Unix path
+    assert_equal Pathname("/foo/bar"), to_pathname("file:///foo/bar")
+    assert_equal Pathname("/foo/bar"), to_pathname("file:/foo/bar")
+
+    # Dosish path
+    assert_equal Pathname("C:/foo/bar"), to_pathname("file:///C:/foo/bar", dosish: true)
+
+    # Non file: URI
+    assert_nil to_pathname("http://foo/bar.baz")
+    assert_nil to_pathname("untitled:Untitled-1")
+  end
+
+  def test_to_uri
+    assert_equal URI("file:///foo/bar"), to_uri(Pathname("/foo/bar"))
+    assert_equal URI("file:///c:/foo/bar"), to_uri(Pathname("c:/foo/bar"), dosish: true)
+  end
+end
