352705eecbdb1edb137468d64ee776ce3268a26d
Soutaro Matsumoto <matsumoto@soutaro.com>
2023-01-06 11:04:04 +0900

    Update for collection changes in RBS 3.0

--- lib/steep/project/options.rb
+++ lib/steep/project/options.rb
@@ -1,7 +1,9 @@
 module Steep
   class Project
     class Options
-      PathOptions = Struct.new(:core_root, :stdlib_root, :repo_paths, keyword_init: true) do
+      PathOptions = _ = Struct.new(:core_root, :stdlib_root, :repo_paths, keyword_init: true) do
+        # @implements PathOptions
+        
         def customized_stdlib?
           stdlib_root != nil
         end
