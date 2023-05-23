7ffdf1fdd44d155e83705337e35afb3f07ffcc38
Takeshi KOMIYA <i.tkomiya@gmail.com>
2023-05-24 03:00:35 +0900

    Add Steep::Diagnostic::Ruby.silent template to suppress all warnings
    To use steep for completions and navigations, this adds a new settings
    template `Steep::Diagnostic::Ruby.silent` that suppresses all warnings.
    
    refs: #787

--- lib/steep/drivers/init.rb
+++ lib/steep/drivers/init.rb
@@ -23,6 +23,7 @@ module Steep
       #
       #   # configure_code_diagnostics(D::Ruby.strict)       # `strict` diagnostics setting
       #   # configure_code_diagnostics(D::Ruby.lenient)      # `lenient` diagnostics setting
+      #   # configure_code_diagnostics(D::Ruby.silent)       # `silent` diagnostics setting
       #   # configure_code_diagnostics do |hash|             # You can setup everything yourself
       #   #   hash[D::Ruby::NoMethod] = :information
       #   # end
