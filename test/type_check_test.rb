719edc6785a83e4eca9024877ab6637d6df71a55
Soutaro Matsumoto <matsumoto@soutaro.com>
2023-05-31 14:34:36 +0900

    Add test

---
+++ test/type_check_test.rb
@@ -0,0 +1,106 @@
+require_relative "test_helper"
+
+class TypeConstructionTest < Minitest::Test
+  include TestHelper
+  include TypeErrorAssertions
+  include FactoryHelper
+  include SubtypingHelper
+  include TypeConstructionHelper
+
+  include Steep
+
+  def run_type_check_test(signatures: {}, code: {}, expectations: nil)
+    typings = {}
+
+    with_factory(signatures, nostdlib: false) do |factory|
+      builder = Interface::Builder.new(factory)
+      subtyping = Subtyping::Check.new(builder: builder)
+
+      code.each do |path, content|
+        source = Source.parse(content, path: path, factory: factory)
+        with_standard_construction(subtyping, source) do |construction, typing|
+          construction.synthesize(source.node)
+
+          typings[path] = typing
+        end
+      end
+    end
+
+    formatter = Diagnostic::LSPFormatter.new()
+
+    diagnostics = typings.transform_values do |typing|
+      typing.errors.map do |error|
+        Expectations::Diagnostic.from_lsp(formatter.format(error))
+      end
+    end
+
+    if expectations
+      exps = Expectations.empty
+      diagnostics.each do |path, ds|
+        exps.diagnostics[path] = ds
+      end
+      exps.to_yaml
+
+      assert_equal expectations, exps.to_yaml
+    end
+
+    yield typings if block_given?
+  end
+
+  def test_setter_type
+    run_type_check_test(
+      signatures: {
+        "a.rbs" => <<~RBS
+          class SetterReturnType
+            def foo=: (String) -> String
+          end
+        RBS
+      },
+      code: {
+        "a.rb" => <<~RUBY
+          class SetterReturnType
+            def foo=(value)
+              if value
+                return
+              else
+                123
+              end
+            end
+          end
+        RUBY
+      },
+      expectations: <<~YAML
+        ---
+        - file: a.rb
+          diagnostics:
+          - range:
+              start:
+                line: 2
+                character: 6
+              end:
+                line: 2
+                character: 10
+            severity: ERROR
+            message: |-
+              Setter method `foo=` cannot have type `::Integer` because declared as type `::String`
+                ::Integer <: ::String
+                  ::Numeric <: ::String
+                    ::Object <: ::String
+                      ::BasicObject <: ::String
+            code: Ruby::SetterBodyTypeMismatch
+          - range:
+              start:
+                line: 4
+                character: 6
+              end:
+                line: 4
+                character: 12
+            severity: ERROR
+            message: |-
+              The setter method `foo=` cannot return a value of type `nil` because declared as type `::String`
+                nil <: ::String
+            code: Ruby::SetterReturnTypeMismatch
+      YAML
+    )
+  end
+end
