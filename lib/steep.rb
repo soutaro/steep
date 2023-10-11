68947aa8219286b44bbe1c09f8604dc210396de1
Soutaro Matsumoto <matsumoto@soutaro.com>
2023-10-11 22:46:23 +0900

    Delete shims

--- lib/steep.rb
+++ lib/steep.rb
@@ -23,9 +23,6 @@ require "rbs"
 
 require "steep/path_helper"
 
-require "steep/shims/filter_map"
-require "steep/shims/symbol_start_with"
-
 require "steep/thread_waiter"
 require "steep/equatable"
 require "steep/method_name"
