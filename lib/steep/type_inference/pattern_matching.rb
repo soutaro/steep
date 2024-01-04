984ad96342a0f881e7723e24bb12f1f643a2158f
Soutaro Matsumoto <matsumoto@soutaro.com>
2024-01-04 13:07:11 +0900

    :cat2:

--- lib/steep/type_inference/pattern_matching.rb
+++ lib/steep/type_inference/pattern_matching.rb
@@ -1,14 +1,13 @@
 module Steep
   module TypeInference
     class PatternMatching
-      class Clause < Struct.new(:truthy_result, :falsy_result, :clause_type)
+      class Clause < Struct.new(:truthy_result, :falsy_result, :body_pair)
       end
 
       attr_reader :initial, :clauses, :last_result
 
-      def initialize(initial:, initial_result:)
+      def initialize(initial:)
         @initial = initial
-        @last_result = initial_result
         @clauses = []
       end
 
@@ -23,11 +22,38 @@ module Steep
       end
 
       def else_clause()
-
+        @else_clause_result = pair = yield last_result.env
       end
 
       def result
+        types = [] #: Array[AST::Types::t]
+        envs = [] #: Array[TypeEnv]
 
+        clauses.each do |clause|
+          types << clause.body_result.type
+          envs << clause.body_result.constr.context.type_env
+        end
+        if else_result = @else_clause_result
+          types << else_result.type
+          envs << else_result.constr.context.type_env
+        end
+
+        type = AST::Types::Union.build(types: types)
+        env = initial.constr.context.type_env.join(*envs)
+
+        TypeConstruction::Pair.new(type: type, constr: initial.constr.update_type_env { env })
+      end
+
+      def last_result
+        if last_clause = clauses.last
+          last_clause.falsy_result
+        else
+          LogicTypeInterpreter::Result.new(
+            env: initial.constr.context.type_env,
+            type: AST::Builtin.any_type,
+            unreachable: false
+          )
+        end
       end
     end
   end
