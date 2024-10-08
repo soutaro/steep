8e3672a427d61a0db2d013a6ccbfe9fb4c341e0d
Soutaro Matsumoto <matsumoto@soutaro.com>
2024-10-08 20:45:04 +0900

    Fix output tests

--- smoke/tsort/a.rb
+++ smoke/tsort/a.rb
@@ -6,7 +6,7 @@ g = {1=>[2, 3], 2=>[4], 3=>[2, 4], 4=>[]}
 # @type var each_node: ^() { (Integer) -> void } -> void
 each_node = -> (&b) { g.each_key(&b) }
 # @type var each_child: ^(Integer) { (Integer) -> void } -> void
-each_child = -> (n, &b) { g[n].each(&b) }
+each_child = -> (n, &b) { g[n]&.each(&b) }
 
 # @type var xs: Array[Integer]
 xs = TSort.tsort(each_node, each_child)
